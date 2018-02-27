8834b47b68701c3a3de7d6276aa5a80c47962dbe
Denis Rystsov <rystsov.denis@gmail.com>
2018-02-26 20:48:59 -0800

    Removes MongoDB's client

--- performance/mongodb/client/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-date | tee /mongo/logs/client1.log
-nodejs app/src/test.js $@ 2>&1 | tee -a /mongo/logs/client1.log
--- performance/mongodb/db/remote-tester/src/start.js
+++ performance/mongodb/db/remote-tester/src/start.js
@@ -3,6 +3,6 @@ const {RemoteTesterServer} = require("./RemoteTesterServer");
 
 const service = new RemoteTesterServer(
     new MongoKV("mongodb://node1,node2,node3/?replicaSet=rs0&autoReconnect=false&socketTimeoutMS=10000&connectTimeoutMS=10000"),
-    2379
+    8000
 );
 service.start();
